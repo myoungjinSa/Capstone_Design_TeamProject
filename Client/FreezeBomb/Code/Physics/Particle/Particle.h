144187a01fa67e79ba6ccb9721d0f4ca7eb958b7
Sa Myoung Jin <ekdskrnl105@gmail.com>
2019-06-28 21:05:09 +0900

    파티클 클래스 생성 및 Firework 객체 정의
    추후에 Firework 파티클을 구현 시도해 볼 예정

--- Client/FreezeBomb/Code/Physics/Particle/Particle.h
+++ Client/FreezeBomb/Code/Physics/Particle/Particle.h
@@ -121,6 +121,153 @@ namespace Physics
 
 		public:
 
+			/**
+		    * Integrates the particle forward in time by the given amount.
+			* This function uses a Newton-Euler integration method, which is a
+			* linear approximation to the correct integral. For this reason it
+			* may be inaccurate in some cases.
+			*/
+			void Intergrate(real duration);
+
+			/**
+			* Sets the mass of the particle.
+			*
+			 * @param mass The new mass of the body. This may not be zero.
+			 * Small masses can produce unstable rigid bodies under
+			 * simulation.
+			 *
+			 * @warning This invalidates internal data for the particle.
+			 * Either an integration function, or the calculateInternals
+			 * function should be called before trying to get any settings
+			 * from the particle.
+			 */
+			void SetMass(const real mass);
+
+
+			 /**
+			 * Gets the mass of the particle.
+			*
+			 * @return The current mass of the particle.
+			 */
+			real GetMass() const;
+
+
+			  /**
+			* Sets the inverse mass of the particle.
+			*
+			 * @param inverseMass The new inverse mass of the body. This
+			 * may be zero, for a body with infinite mass
+			 * (i.e. unmovable).
+			 *
+			 * @warning This invalidates internal data for the particle.
+			 * Either an integration function, or the calculateInternals
+			 * function should be called before trying to get any settings
+			 * from the particle.
+			 */
+			void SetInverseMass(const real inverseMass);
+
+			        /**
+         * Gets the inverse mass of the particle.
+         *
+         * @return The current inverse mass of the particle.
+         */
+        real GetInverseMass() const;
+
+	    /**
+        * Returns true if the mass of the particle is not-infinite.
+        */
+        bool HasFiniteMass() const;
+
+
+		 /**
+         * Sets both the damping of the particle.
+         */
+        void SetDamping(const real damping);
+
+        /**
+         * Gets the current damping value.
+         */
+        real GetDamping() const;
+
+		/**
+        * Sets the position of the particle.
+        *
+        * @param position The new position of the particle.
+        */
+        void SetPosition(const PVector3 &position);
+
+
+		 /**
+         * Sets the position of the particle by component.
+         *
+         * @param x The x coordinate of the new position of the rigid
+         * body.
+         *
+         * @param y The y coordinate of the new position of the rigid
+         * body.
+         *
+         * @param z The z coordinate of the new position of the rigid
+         * body.
+         */
+        void SetPosition(const real x, const real y, const real z);
+
+		 /**
+         * Fills the given vector with the position of the particle.
+         *
+         * @param position A pointer to a vector into which to write
+         * the position.
+         */
+        void GetPosition(PVector3 *position) const;
+
+
+		 /**
+         * Gets the position of the particle.
+         *
+         * @return The position of the particle.
+         */
+        PVector3 GetPosition() const;
+
+
+		 /**
+         * Sets the velocity of the particle.
+         *
+         * @param velocity The new velocity of the particle.
+         */
+        void SetVelocity(const PVector3 &velocity);
+
+		void SetVelocity(const real x, const real y, const real z);
+
+		void GetVelocity(PVector3 *velocity) const;
+
+		PVector3 GetVelocity() const;
+
+		 /**
+         * Sets the constant acceleration of the particle.
+         *
+         * @param acceleration The new acceleration of the particle.
+         */
+        void SetAcceleration(const PVector3 &acceleration);
+
+		void SetAcceleration(const real x, const real y, const real z);
+
+		void GetAcceleration(PVector3 *acceleration) const;
+
+		PVector3 GetAcceleration() const;
+
+		/**
+         * Clears the forces applied to the particle. This will be
+         * called automatically after each integration step.
+         */
+        void ClearAccumulator();
+
+		/**
+        * Adds the given force to the particle, to be applied at the
+        * next iteration only.
+        *
+        * @param force The force to apply.
+        */
+        void AddForce(const PVector3 &force);
+
 	};
 
 }
